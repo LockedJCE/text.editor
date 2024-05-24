640ff3b7cc222d2dbcefdb51a2c44ee1b3cbc5e6
Jensen <163614828+LockedJCE@users.noreply.github.com>
2024-05-23 21:42:02 -0700

    will it deploy?

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,6 @@
 import { openDB } from "idb";
 
+// start the database and checks if exists, if not create one
 const initdb = async () =>
   openDB("jate", 1, {
     upgrade(db) {
@@ -12,26 +13,31 @@ const initdb = async () =>
     },
   });
 
-// Logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log("PUT to the database");
+  console.log("Put to the database");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ id: 1, content: content });
+  const request = store.put({
+    id: 1,
+    value: content,
+  });
   const result = await request;
-  console.log("🚀 - data saved to the database", result.content);
+  console.log("Data saved to the daatabase", result.value);
 };
-// Logic for a method that gets all the content from the database
+
+// Get data from the indexedb database
 export const getDb = async () => {
   console.log("GET from the database");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
-  console.log("result.value", result.content);
-  return result?.content;
+  result
+    ? console.log("Result value: ", result.value)
+    : console.log("No data found in the db");
+  return result?.value;
 };
 
 initdb();
