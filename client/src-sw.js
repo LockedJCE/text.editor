680432c56b482daa1c454127bb0a0843b5b1ca8c
Jensen <163614828+LockedJCE@users.noreply.github.com>
2024-05-27 19:34:02 -0700

    fixes

--- client/src-sw.js
+++ client/src-sw.js
@@ -5,8 +5,10 @@ const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
 const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
+// Precaching the assets specified in the manifest
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Setting up the page cache strategy
 const pageCache = new CacheFirst({
   cacheName: "page-cache",
   plugins: [
@@ -14,27 +16,32 @@ const pageCache = new CacheFirst({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
     }),
   ],
 });
 
+// Warming up the cache with important URLs
 warmStrategyCache({
   urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
+// Registering a route to use the page cache strategy for navigation requests
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// Asset caching
+// TODO: Implement asset caching
+// Implementing asset caching for styles, scripts, and workers
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
-
   new StaleWhileRevalidate({
     cacheName: "asset-cache",
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
       }),
     ],
   })
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -15,31 +15,28 @@ module.exports = () => {
       path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      // Generates the HTML file
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "Text Editor",
+        title: "J.A.T.E",
       }),
-      // Injects our custom service worker
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
-      // Creates a manifest file
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Text Editor",
-        short_name: "TextEdit",
-        description: "A simple text editor PWA!",
-        background_color: "#ffffff",
-        theme_color: "#317EFB",
-        start_url: "/",
-        publicPath: "/",
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "Takes notes with Javascript syntax highlighting!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join("assets", "icons"),
           },
         ],
@@ -48,12 +45,10 @@ module.exports = () => {
 
     module: {
       rules: [
-        // CSS loaders
         {
           test: /\.css$/i,
           use: ["style-loader", "css-loader"],
         },
-        // Babel loader
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
@@ -61,7 +56,10 @@ module.exports = () => {
             loader: "babel-loader",
             options: {
               presets: ["@babel/preset-env"],
-              plugins: ["@babel/plugin-transform-runtime"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
